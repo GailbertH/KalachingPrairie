6518ffb3755b8fc15ceea12dbeff8f24627255f6
Alex Pascal <j.pascalex@gmail.com>
2018-04-01 03:59:28 +0800

    Game Prototyping.

--- Assets/Resources/Script/Plant/PlantHandler.cs
+++ Assets/Resources/Script/Plant/PlantHandler.cs
@@ -6,9 +6,25 @@ public class PlantHandler : MonoBehaviour
 {
 	[SerializeField] List<PlantData> plantData;
 	[SerializeField] List<PlantController> plantController;
+	[SerializeField] Sprite soil;
+	[SerializeField] Sprite deadPlant;
 
+	public PlantController targetPlant;
+	public Sprite GetEmptySoilForm
+	{
+		get{ return soil;}
+	}
 	public void AddPlant(PlantType type)
 	{
-		//Add Plan
+		targetPlant.SetUpPlanting (plantData [(int)type]);
+	}
+
+	public void DayCycle()
+	{
+		for(int i = 0; i < plantController.Count; i++)
+		{
+			plantController [i].ReduceCountDown ();
+			plantController [i].ChangePlantNeedState (PlantNeedState.WATER);
+		}
 	}
 }
--- Assets/Resources/Script/PlayerHandler.cs
+++ Assets/Resources/Script/PlayerHandler.cs
@@ -1,17 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerHandler : MonoBehaviour {
 
 	[SerializeField] GameObject Player;
-	private float vertical = 0;
-	private float horizontal = 0;
-
+	private PlayerData playerData;
+	private ItemEquip equipedItem = ItemEquip.WATERING_CAN;
+	private PlantType seedType = PlantType.STRAWBERRY;
+	private int EquipItemTracker = 0;
+	private int SeedEquipTracker = 0;
 	// Update is called once per frame
+	public ItemEquip GetItemEquip
+	{
+		get{ return equipedItem;}
+	}
+	public PlantType GetSeedType
+	{
+		get{ return seedType;}
+	}
+
+	public void ChangeItemEquip()
+	{
+		if (EquipItemTracker == (int)ItemEquip.SEED) 
+		{
+			if (SeedEquipTracker == (int)PlantType.RICE) 
+			{
+				EquipItemTracker = 0;
+				SeedEquipTracker = 0;
+				seedType = PlantType.STRAWBERRY;
+				equipedItem = ItemEquip.WATERING_CAN;
+				return;
+			} 
+			else 
+			{
+				SeedEquipTracker += 1;
+				seedType = (PlantType)SeedEquipTracker;
+				return;
+			}
+		}
+		EquipItemTracker += 1;
+		equipedItem = (ItemEquip)EquipItemTracker;
+	}
+
 	void FixedUpdate () 
 	{
-		if (GameManager.Instance == null)
+		if (GameManager.Instance == null )
+			return;
+
+		if (GameManager.Instance.GameControls == null)
 			return;
 
 		Vector2 newPos = GameManager.Instance.GameControls.GetTouchInput;
@@ -19,7 +57,7 @@ public class PlayerHandler : MonoBehaviour {
 			newPos.y = this.transform.position.y;
 		
 		Vector2 curPos = new Vector2 (this.transform.position.x, this.transform.position.y);
-		Player.transform.localPosition = Vector2.MoveTowards(curPos, newPos, 0.05f);
-			
+		//Player.transform.localPosition = Vector2.MoveTowards(curPos, newPos, 0.05f);
+		Player.GetComponent<Rigidbody2D> ().MovePosition (Vector2.MoveTowards(curPos, newPos, 0.05f));
 	}
 }
